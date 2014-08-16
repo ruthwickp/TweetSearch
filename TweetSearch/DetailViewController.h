cf3dc44056d181ab2b86897ed84c91aeb32c2452
ruthwickp <ruthwickp@yahoo.com>
2014-08-16 07:30:33 -0700

    Initial Commit

---
+++ TweetSearch/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  TweetSearch
+//
+//  Created by Ruthwick Pathireddy on 8/16/14.
+//  Copyright (c) 2014 Darkking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ TweetSearch/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  TweetSearch
+//
+//  Created by Ruthwick Pathireddy on 8/16/14.
+//  Copyright (c) 2014 Darkking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
